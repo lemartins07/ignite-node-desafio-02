021ceed705c88a7acef213c3b67cdbd1c7e3b991
Leandro Martins <leandro.martins.85@gmail.com>
2023-04-05 16:04:13 -0300

    Database configured

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { knex } from './database'
 
 export const app = fastify()
+
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
